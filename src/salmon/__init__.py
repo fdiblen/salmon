7fa1e2fb29c575301361c31949f322cae72b3e57
Faruk D. <fdiblen@universe>
2024-12-09 13:38:21 +0100

    added my new package

---
+++ src/salmon/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about salmon."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "FAruk Diblen"
+__email__ = "fdiblen@nlesc.nl"
+__version__ = "0.1.0"
